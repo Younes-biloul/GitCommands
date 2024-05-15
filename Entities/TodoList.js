bdcd9059dc18157833852bec1c596ee2cb2934a4
Younes BILOUL <younes.biloul@jobintech-uir.ma>
2024-05-15 14:56:44 +0100

    first commit

---
+++ Entities/TodoList.js
@@ -0,0 +1,35 @@
+/**
+ * @typedef {Object} ITodolist
+ * @property {number} id
+ * @property {string} title
+ * @property {string} body
+ * @property {string} color
+ */
+
+
+/**
+ * @implements {ITodolist}
+ */
+export class Todolist  {
+    /**
+     * @param {number} id
+     * @param {string} title
+     * @param {string} body
+     * @param {string} color
+     */
+    constructor(id, title, body, color) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.color = color;
+    }
+    
+    
+    printDetails() {
+        console.log(`ID: ${this.id}, Title: ${this.title}, Body: ${this.body}, Color: ${this.color}`);
+    }
+}
+
+// Example usage:
+const myTodo = new Todolist(1, "My Title", "This is the body", "blue");
+myTodo.printDetails();
